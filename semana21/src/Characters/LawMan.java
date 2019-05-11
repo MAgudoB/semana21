be8e1795264400eba94c915258d6ab6892dcd0bc
Bertum <albertodelcru@gmail.com>
2019-05-11 13:37:08 +0200

    Lawman Movement and camera

--- semana21/src/Characters/LawMan.java
+++ semana21/src/Characters/LawMan.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//https://sites.google.com/site/java3dapplets/home/source-code-move-tinkerbell
 package Characters;
 
+import java.util.Random;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
@@ -14,21 +9,54 @@ import javax.vecmath.Vector3d;
 
 /**
  *
- * @author Miguel
+ * @author Bertum
  */
-public class Raptor extends BranchGroup implements CharacterMovement {
+public class LawMan extends BranchGroup implements CharacterMovement {
 
     private boolean turnRight, turnLeft, goFront, goBack;
     private double angleY = Math.toRadians(15);
-    private double speed = 0.2;
+    private double speed = 0.1;
     private Matrix4d matrix = new Matrix4d();
 
-    public Raptor() {
-        super();
+    //Get distance
+    private double getDistance(float x1, float y1,
+            float z1, float x2,
+            float y2, float z2) {
+
+        double d = Math.pow((Math.pow(x2 - x1, 2)
+                + Math.pow(y2 - y1, 2)
+                + Math.pow(z2 - z1, 2)
+                * 1.0), 0.5);
+        System.out.println("Distance is " + d);
+        return d;
+    }
+
+    private void getRandomMove() {
+        Random rnd = new Random();
+        int num = rnd.nextInt(3 - 0 + 1) + 0;
+        turnRight = false;
+        turnLeft = false;
+        goFront = false;
+        goBack = false;
+        switch (num) {
+            case 0:
+                turnRight = true;
+                break;
+            case 1:
+                turnLeft = true;
+                break;
+            case 2:
+                goFront = true;
+                break;
+            case 3:
+                goBack = true;
+                break;
+        }
     }
 
     @Override
     public void move() {
+        getRandomMove();
         TransformGroup tg = (TransformGroup) this.getChild(0);
         Transform3D t3d = new Transform3D();
         Transform3D t3dStep = new Transform3D();
@@ -61,21 +89,5 @@ public class Raptor extends BranchGroup implements CharacterMovement {
             t3d.mul(t3dStep);
             tg.setTransform(t3d);
         }
-    }
-
-    public void setTurnRight(boolean turnRight) {
-        this.turnRight = turnRight;
-    }
-
-    public void setTurnLeft(boolean turnLeft) {
-        this.turnLeft = turnLeft;
-    }
-
-    public void setGoFront(boolean goFront) {
-        this.goFront = goFront;
-    }
-
-    public void setGoBack(boolean goBack) {
-        this.goBack = goBack;
     }
 }
--- semana21/src/semana21/Semana21.java
+++ semana21/src/semana21/Semana21.java
@@ -1,173 +1,99 @@
 package semana21;
 
+import Characters.LawMan;
 import Characters.Raptor;
 import com.sun.j3d.utils.picking.*;
-
 import com.sun.j3d.utils.universe.SimpleUniverse;
-
 import com.sun.j3d.utils.geometry.*;
-
 import com.sun.j3d.loaders.*;
 import com.sun.j3d.loaders.objectfile.ObjectFile;
-
 import javax.media.j3d.*;
-
 import javax.vecmath.*;
-
 import java.awt.event.*;
-
 import java.awt.*;
 import java.io.File;
-
+import java.util.ArrayList;
+import java.util.Random;
 import javax.swing.Timer;
 
 public class Semana21 extends MouseAdapter implements ActionListener, KeyListener {
 
-private PickCanvas pickCanvas;
-private Timer timer;
-private boolean turnRight,turnLeft,goFront,goBack;
-private Raptor raptor;
+    private int numLawMans = 3;
+    private PickCanvas pickCanvas;
+    private Timer timer;
+    private boolean turnRight, turnLeft, goFront, goBack;
+    private Raptor raptor;
+    private TransformGroup viewingTransform;
+    private ArrayList<LawMan> lawMans = new ArrayList<LawMan>();
+    private SimpleUniverse universe;
 
+    public Semana21() {
+        Frame frame = new Frame("Jurasic World");
+        timer = new Timer(100, this);
 
-public Semana21() {
+        GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+        Canvas3D canvas = new Canvas3D(config);
+        canvas.setSize(800, 800);
+        universe = new SimpleUniverse(canvas);
 
-    Frame frame = new Frame("Jurasic World");
-    
-    timer = new Timer(100,this);
-      
+        BranchGroup group = new BranchGroup();
+        raptor = createRaptor();
+        group.addChild(raptor);
 
-    GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+        for (int i = 0; i < numLawMans; i++) {
+            Random rnd = new Random();
+            LawMan newLawMan = createLawMan(rnd.nextFloat() * i * 10, rnd.nextFloat() * i * 10);
+            lawMans.add(newLawMan);
+            group.addChild(newLawMan);
+        }
 
-    Canvas3D canvas = new Canvas3D(config);
+        universe.getViewingPlatform().setNominalViewingTransform();
+        universe.addBranchGraph(group);
 
-    canvas.setSize(800, 800);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent winEvent) {
+                System.exit(0);
+            }
+        });
+        frame.add(canvas);
 
-    SimpleUniverse universe = new SimpleUniverse(canvas);
+        pickCanvas = new PickCanvas(canvas, group);
+        pickCanvas.setMode(PickCanvas.BOUNDS);
+        canvas.addMouseListener(this);
+        canvas.addKeyListener(this);
 
-    BranchGroup group = new BranchGroup();    
-    
-    raptor = createRaptor();
-    
-    group.addChild(raptor);
-    
-    group.addChild(createLawMan());
-
-    universe.getViewingPlatform().setNominalViewingTransform();
-
-    universe.addBranchGraph(group);
-
-    frame.addWindowListener(new WindowAdapter() {
-
-       public void windowClosing(WindowEvent winEvent) {
-
-          System.exit(0);
-
-       }
-
-    });
-
-    frame.add(canvas);
-
-    pickCanvas = new PickCanvas(canvas, group);
-
-    pickCanvas.setMode(PickCanvas.BOUNDS);
-
-    canvas.addMouseListener(this);
-    
-    canvas.addKeyListener(this);
-
-    frame.pack();
-
-    frame.setVisible(true);
-    
-    timer.start();
-
-}
-
-public static void main( String[] args ) {
-
-    System.setProperty("sun.awt.noerasebackground", "true");
-
-    Semana21 game = new Semana21();
-    
-    
-
-}
-
-public void mouseClicked(MouseEvent e)  {
-
-    pickCanvas.setShapeLocation(e);
-
-    PickResult result = pickCanvas.pickClosest();
-
-    if (result == null) {
-
-       System.out.println("Nothing picked");
-
-    } else {
-
-       Primitive p = (Primitive)result.getNode(PickResult.PRIMITIVE);
-
-       Shape3D s = (Shape3D)result.getNode(PickResult.SHAPE3D);
-
-       if (p != null) {
-
-          System.out.println(p.getClass().getName());
-
-       } else if (s != null) {
-
-             System.out.println(s.getClass().getName());
-
-       } else{
-
-          System.out.println("null");
-
-       }
-    }
-}
-
-    private void createCube(BranchGroup group) {
-        Vector3f vector = new Vector3f(-0.3f, 0.0f, 0.0f);
-
-        Transform3D transform = new Transform3D();
-
-        transform.setTranslation(vector);
-
-        TransformGroup transformGroup = new TransformGroup(transform);
-
-        ColorCube cube = new ColorCube(0.3);
-
-        transformGroup.addChild(cube);
-
-        group.addChild(transformGroup);
+        frame.pack();
+        frame.setVisible(true);
+        timer.start();
     }
 
-    private void createSphere(BranchGroup group) {
-        Vector3f vector2 = new Vector3f(+0.3f, 0.0f, 0.0f);
+    public static void main(String[] args) {
+        System.setProperty("sun.awt.noerasebackground", "true");
+        Semana21 game = new Semana21();
+    }
 
-        Transform3D transform2 = new Transform3D();
+    public void mouseClicked(MouseEvent e) {
+        pickCanvas.setShapeLocation(e);
+        PickResult result = pickCanvas.pickClosest();
 
-        transform2.setTranslation(vector2);
+        if (result == null) {
+            System.out.println("Nothing picked");
+        } else {
+            Primitive p = (Primitive) result.getNode(PickResult.PRIMITIVE);
+            Shape3D s = (Shape3D) result.getNode(PickResult.SHAPE3D);
 
-        TransformGroup transformGroup2 = new TransformGroup(transform2);
-
-        Appearance appearance = new Appearance();
-
-        appearance.setPolygonAttributes(
-
-           new PolygonAttributes(PolygonAttributes.POLYGON_LINE,
-
-           PolygonAttributes.CULL_BACK,0.0f));
-
-        Sphere sphere = new Sphere(0.3f,appearance);
-
-        transformGroup2.addChild(sphere);
-
-        group.addChild(transformGroup2);
+            if (p != null) {
+                System.out.println(p.getClass().getName());
+            } else if (s != null) {
+                System.out.println(s.getClass().getName());
+            } else {
+                System.out.println("null");
+            }
+        }
     }
 
     private Raptor createRaptor() {
-        
+
         Raptor objRoot = new Raptor();
         TransformGroup tg = new TransformGroup();
         Transform3D t3d = new Transform3D();
@@ -186,10 +112,10 @@ public void mouseClicked(MouseEvent e)  {
         File file = new java.io.File("src//semana21//Irex_obj.obj");
 
         try {
-         s = loader.load(file.toURI().toURL());
+            s = loader.load(file.toURI().toURL());
         } catch (Exception e) {
-         System.err.println(e);
-         System.exit(1);
+            System.err.println(e);
+            System.exit(1);
         }
 
         tg.addChild(s.getSceneGroup());
@@ -199,16 +125,15 @@ public void mouseClicked(MouseEvent e)  {
         objRoot.compile();
         return objRoot;
     }
-    
-    private BranchGroup createLawMan() {
-        
-        BranchGroup objRoot = new BranchGroup();
+
+    private LawMan createLawMan(float x, float z) {
+        LawMan objRoot = new LawMan();
         TransformGroup tg = new TransformGroup();
         Transform3D t3d = new Transform3D();
 
         tg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 
-        t3d.setTranslation(new Vector3f(+0.3f, 0.0f, 0.0f));
+        t3d.setTranslation(new Vector3f(x, 0.0f, z));
         t3d.setRotation(new AxisAngle4f(0.0f, 0.0f, 0.0f, 0.0f));
         t3d.setScale(1.0);
 
@@ -220,10 +145,10 @@ public void mouseClicked(MouseEvent e)  {
         File file = new java.io.File("src//semana21//lawMan.obj");
 
         try {
-         s = loader.load(file.toURI().toURL());
+            s = loader.load(file.toURI().toURL());
         } catch (Exception e) {
-         System.err.println(e);
-         System.exit(1);
+            System.err.println(e);
+            System.exit(1);
         }
 
         tg.addChild(s.getSceneGroup());
@@ -234,60 +159,85 @@ public void mouseClicked(MouseEvent e)  {
 
         return objRoot;
     }
-    
+
     private Light createLight() {
-      DirectionalLight light = new DirectionalLight(true, new Color3f(1.0f,
-        1.0f, 1.0f), new Vector3f(-0.3f, 0.2f, -1.0f));
+        DirectionalLight light = new DirectionalLight(true, new Color3f(1.0f,
+                1.0f, 1.0f), new Vector3f(0.3f, -10, 0));
+        light.setInfluencingBounds(new BoundingSphere(new Point3d(), 10000.0));
 
-      light.setInfluencingBounds(new BoundingSphere(new Point3d(), 10000.0));
+        return light;
+    }
 
-      return light;
-     }
+    private void moveCamera() {
+        TransformGroup viewingTransformGroup;
+        viewingTransformGroup = universe.getViewingPlatform().getViewPlatformTransform();
+        Transform3D viewingTransform = new Transform3D();
 
-    //Bucle
+        TransformGroup tg = (TransformGroup) raptor.getChild(0);
+        Transform3D t3d = new Transform3D();
+        tg.getTransform(t3d);
+        Vector3d vector = new Vector3d();
+        t3d.get(vector);
+        Point3d point = new Point3d();
+        point.x = vector.x;
+        point.y = vector.y;
+        point.z = vector.z;
+        viewingTransform.lookAt(new Point3d(point.x, point.y + 2, point.z - 3), point,
+                new Vector3d(0, 1, 0));
+
+        viewingTransform.invert();
+        viewingTransformGroup.setTransform(viewingTransform);
+    }
+
+//Bucle
     @Override
     public void actionPerformed(ActionEvent ae) {
         raptor.move();
+        moveCamera();
+        for (LawMan lawMan : lawMans) {
+            lawMan.move();
+        }
     }
 
     @Override
     public void keyTyped(KeyEvent ke) {
-        System.out.println("Key Typed: "+ke.getKeyCode());
+        System.out.println("Key Typed: " + ke.getKeyCode());
     }
+
     //Pulsar una tecla
     @Override
     public void keyPressed(KeyEvent ke) {
-        if(ke.getKeyChar()=='a'){
-        	raptor.setTurnLeft(true);
-        	raptor.setTurnRight(false);
+        if (ke.getKeyChar() == 'a') {
+            raptor.setTurnLeft(true);
+            raptor.setTurnRight(false);
         }
-        if(ke.getKeyChar()=='w'){
-        	raptor.setGoFront(true);
-        	raptor.setGoBack(false);
+        if (ke.getKeyChar() == 'w') {
+            raptor.setGoFront(true);
+            raptor.setGoBack(false);
         }
-        if(ke.getKeyChar()=='s'){
-        	raptor.setGoFront(false);
-        	raptor.setGoBack(true);
+        if (ke.getKeyChar() == 's') {
+            raptor.setGoFront(false);
+            raptor.setGoBack(true);
         }
-        if(ke.getKeyChar()=='d'){
-        	raptor.setTurnLeft(false);
-        	raptor.setTurnRight(true);
+        if (ke.getKeyChar() == 'd') {
+            raptor.setTurnLeft(false);
+            raptor.setTurnRight(true);
         }
     }
 
     @Override
     public void keyReleased(KeyEvent ke) {
-        if(ke.getKeyChar()=='a'){
-        	raptor.setTurnLeft(false);
+        if (ke.getKeyChar() == 'a') {
+            raptor.setTurnLeft(false);
         }
-        if(ke.getKeyChar()=='w'){
-        	raptor.setGoFront(false);
+        if (ke.getKeyChar() == 'w') {
+            raptor.setGoFront(false);
         }
-        if(ke.getKeyChar()=='s'){
-        	raptor.setGoBack(false);
+        if (ke.getKeyChar() == 's') {
+            raptor.setGoBack(false);
         }
-        if(ke.getKeyChar()=='d'){
-        	raptor.setTurnRight(false);
+        if (ke.getKeyChar() == 'd') {
+            raptor.setTurnRight(false);
         }
     }
 }
